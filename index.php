97a69003b60176a38625a6b7853bbad13fa186fa
Leonardo Briguente <leobriguente27@gmail.com>
2024-11-07 12:36:11 -0300

    AB#22

--- index.php
+++ index.php
@@ -6,41 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/553722128b.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="view/assets/css/index.css">
-    <script type="text/javascript" src="./assets/javascript/barraNavegacao.js" defer></script>
     <title>Página Inicial</title>
 </head>
 
 <body>
-    <header class="header">
-        <div class="logo-bar">
-            <!-- IDEIA: Fazer um hambumber animado ao clicar. -->
-            <i id="hamburguer" class="fa-solid fa-bars"></i>
+    <?php require "view/header.php"; ?>
 
-            <h2 class="title" style="justify-self: center;"><a href="index.php" id="title">Stima Studio Beauty</a></h2>
-
-        </div>
-
-
-        <nav class="navbar">
-            <!-- IDEIA: Barra lateral esquerda que se sobrepõe a agenda-->
-
-            <div class="sidebar">
-                <ul class="menu">
-                    <!-- IDEIA: Colocar icons -->
-                    <li class="topico"><a class="link" href="./homePage.php">Agenda</a></li>
-                    <li class="topico"><a href="#">Perfil</a></li>
-                    <li class="topico"><a href="#">Clientes</a></li>
-                    <li class="topico"><a href="#">Finaceiro</a></li>
-                </ul>
-            </div>
-
-        </nav>
-    </header>
-
-    <main>
         <h1>TELA BASE DO SISTEMA</h1>
-    </main>
 </body>
 
 </html>
--- view/agenda.php
+++ view/agenda.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/553722128b.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="./assets/css/agenda.css">
+    <!-- <link rel="stylesheet" type="text/css" href="./assets/css/agenda.css"> -->
     <script type="text/javascript" src="./assets/javascript/agenda.js" defer></script>
     <title>Página Inicial</title>
 </head>
