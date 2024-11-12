7bf2dd4aa2baf768cb8652ac3bbeb7a3eaeddc83
KaikyHirai <kaikyyh17@gmail.com>
2024-11-12 16:43:29 -0300

    AB#15 Ajustes nos nomes dos arquivos em modelo camelCase, e ajustado css para melhor responsividade.

--- view/cadastroServicos.php
+++ view/cadastroServicos.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/553722128b.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type ="text/css" href="assets/css/form-servicos.css">
+    <link rel="stylesheet" type ="text/css" href="assets/css/formServicos.css">
     <title>Página Inicial</title>
 </head>
 
