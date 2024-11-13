5ebe3a9c6fa94da7a689fedada28471e7fbe20eb
KaikyHirai <kaikyyh17@gmail.com>
2024-11-13 18:01:44 -0300

    AB#16 Tela de visualização de serviços - first commit

---
+++ view/exibirServicos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/553722128b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type ="text/css" href="assets/css/exibirServicos.css">
+    <title>Visualizar Serviços</title>
+</head>
+
+<body>
+    <?php require "header.php"; ?>
+
+    <div class="content-base">
+        <div class="content-box">
+            <h1>Serviços de Unhas</h1>
+            <h2>Manicure</h2> <!-- vou deixar assim por enquanto, tem que ajustar quando for implementar o backend-->
+        </div>
+
+        <div class="content-box">
+            <h1>Serviços de Cabelo</h1>
+        </div>
+
+        <div class="content-box">
+            <h1>Serviços Estéticos</h1>
+        </div>
+
+        <div class="content-box">
+            <h1>Embelezamento</h1>
+        </div>
+    </div>
+
+</body>
+
+</html>
