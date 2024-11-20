a47dde202b190a44cab1f8b69b72ad094445a8ba
Leonardo Briguente <leobriguente27@gmail.com>
2024-11-20 17:40:47 -0300

    AB#22

--- view/agenda.php
+++ view/agenda.php
@@ -13,38 +13,44 @@
 
 <body>
     <?php require "header.php"; ?>
+    <section class="body">
 
-    <main class="calendar-conteiner">
-        <section class="controll-calendar">
-            <button id="prevMonth">Mês Anterior</button>
-            <h2 id="month-title"></h2>
-            <button id="nextMonth">Próximo Mês</button>
+        <section class="conteudo-principal">
+
+            <main class="calendar-conteiner">
+                <section class="controll-calendar">
+                    <button id="prevMonth">Mês Anterior</button>
+                    <h2 id="month-title"></h2>
+                    <button id="nextMonth">Próximo Mês</button>
+                </section>
+
+                <section class="calendar">
+                    <div id="calendar">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th class="day-name">Dom</th>
+                                    <th class="day-name">Seg</th>
+                                    <th class="day-name">Ter</th>
+                                    <th class="day-name">Qua</th>
+                                    <th class="day-name">Qui</th>
+                                    <th class="day-name">Sex</th>
+                                    <th class="day-name">Sáb</th>
+                                </tr>
+                            </thead>
+                            <tbody id="calendar-body">
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </main>
+
+            <section class="diario">
+                <p>Nenhum evento registrado</p>
+            </section>
         </section>
+    </section>
 
-        <section class="calendar">
-            <div id="calendar">
-                <table>
-                    <thead>
-                        <tr>
-                            <!-- Dias da semana fixos no HTML -->
-                            <th class="day-name">Dom</th>
-                            <th class="day-name">Seg</th>
-                            <th class="day-name">Ter</th>
-                            <th class="day-name">Qua</th>
-                            <th class="day-name">Qui</th>
-                            <th class="day-name">Sex</th>
-                            <th class="day-name">Sáb</th>
-                        </tr>
-                    </thead>
-                    <tbody id="calendar-body">
-                        <!-- Os dias serão inseridos aqui dinamicamente -->
-                    </tbody>
-                </table>
-            </div>
-        </section>
-    </main>
-
-    <section class="diario"></section>
 </body>
 
 </html>
